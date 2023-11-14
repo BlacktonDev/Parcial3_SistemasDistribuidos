c49758ba5bd608ceb3ce69ae66a7896dea65b7b8
TeamN64 <110045405+TeamN64@users.noreply.github.com>
2023-11-13 21:27:06 -0600

    activity 1

--- app/src/main/java/com/example/parcial3/MainActivity.java
+++ app/src/main/java/com/example/parcial3/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.parcial3;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button irSiguienteButton = findViewById(R.id.IniciarButton);
+        Button salirButton = findViewById(R.id.salirButton);
+
+        irSiguienteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, restaurantesActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        salirButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish(); // Cierra la actividad actual y sale de la aplicación.
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/parcial3/restaurantesActivity.java
@@ -0,0 +1,20 @@
+package com.example.parcial3;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class restaurantesActivity extends AppCompatActivity {
+
+
+
+
+
+}
