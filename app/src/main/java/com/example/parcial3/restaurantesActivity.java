695aaf80270c97eabd2ceee32a7572cc9bbe051d
ArturoEscobar-dev <veloctur@gmail.com>
2023-12-05 16:30:11 -0600

    Error de activity corregido

--- app/src/main/java/com/example/parcial3/restaurantesActivity.java
+++ app/src/main/java/com/example/parcial3/restaurantesActivity.java
@@ -12,7 +12,7 @@ public class restaurantesActivity extends AppCompatActivity {
 @Override
     protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    setContentView(R.layout.activity_restaurantes);
 
     Button DonCheloBtn = findViewById(R.id.DonCheloBtn);
     Button GringaBtn = findViewById(R.id.GringaBtn);
