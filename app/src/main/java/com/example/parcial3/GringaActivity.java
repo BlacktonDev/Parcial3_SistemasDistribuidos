ed0a637c71c13a1670b49817b522a56c600ffb09
TeamN64 <110045405+TeamN64@users.noreply.github.com>
2023-12-06 07:42:12 -0600

    la gringa

--- app/src/main/java/com/example/parcial3/GringaActivity.java
+++ app/src/main/java/com/example/parcial3/GringaActivity.java
@@ -1,4 +1,20 @@
 package com.example.parcial3;
 
-public class GringaActivity {
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GringaActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gringa);
+
+        Button backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(v -> {
+
+            onBackPressed();
+        });
+    }
 }
