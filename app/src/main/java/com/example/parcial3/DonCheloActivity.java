c1572df860f1cb04469bb6ac64fd98a7862af943
ArturoEscobar-dev <veloctur@gmail.com>
2023-12-07 09:13:26 -0600

    cambios a don chelo

--- app/src/main/java/com/example/parcial3/DonCheloActivity.java
+++ app/src/main/java/com/example/parcial3/DonCheloActivity.java
@@ -1,6 +1,17 @@
 package com.example.parcial3;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
 
-public class DonCheloActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
 
+public class DonCheloActivity extends AppCompatActivity {
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_donchelo);
+
+
+}
 }
